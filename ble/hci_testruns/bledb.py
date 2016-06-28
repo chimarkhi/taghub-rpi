49a0ed993faf60a9681c875873c3155699fa1f68
sameersingh.mail@gmail.com <krishnarajnairmk@gmail.com>
2016-06-28 12:04:06 +0000

    Initial Commit Sameer Rpi

---
+++ ble/hci_testruns/bledb.py
@@ -0,0 +1,37 @@
+import time
+import sqlite3 as sql
+
+
+## datakeys is a tuple: (macid, dev_name, temp, humidity, tstamp, gateway)
+def createDB(db_path,datakeys):
+	con = sql.connect("db_path")
+	
+	with con:
+		cur = con.cursor()
+		cur.execute("""CREATE TABLE IF NOT EXISTS uploadFIFO \
+		(ID INT PRIMARY KEY AUTOINCREMENT, \
+		datakeys[0] TEXT NOT NULL, \
+		datakeys[1] TEXT NOT NULL, \
+		datakeys[2] REAL, \
+		datakeys[3] REAL, \
+		datakeys[4] TEXT, \
+		datakeys[5] INTEGER, \
+		tstamp DATETIME NOT NULL);""")
+
+                cur.execute("""CREATE TABLE IF NOT EXISTS localData \
+                (ID INT PRIMARY KEY AUTOINCREMENT, \
+                datakeys[0] TEXT NOT NULL, \
+                datakeys[1] TEXT NOT NULL, \
+                datakeys[2] REAL, \
+       	        datakeys[3] REAL, \
+                datakeys[4] TEXT, \
+       	        datakeys[5] INTEGER);""")
+
+def updateDB(db_path, tagData, datakeys)
+        con = sql.connect("db_path")
+	now = datetime.datetime.now()
+        with con:
+                cur = con.cursor()
+		cur.execute("""INSERT INTO uploadFIFO (macid,name,temp,humidity,tstamp,gateway) \
+		values (?,?,?,?,?,?)""",tagData)
+	
---
+++ ble/hci_testruns/test_bleadv.py
@@ -0,0 +1,48 @@
+import time
+import datetime
+import requests
+import os
+import bleadv
+
+## Variables
+scan_iterations=10 # unused
+i=0		
+lib_path="/home/pi/tagbox/ble/hci_testruns/" # dir path
+scantime=10 #scan duration in secs
+sleeptime=120 # sleep duration in secs
+gatewayID=4  # gateway/room ID as specified in server data lists	
+passAll=False # Set true to ignore gatewayID and update/upload all scanned tags
+printUpData=True # Set true for displaying the uploaded data
+
+## temp file names
+advDataFile='./ble_data_parsed.txt'
+
+## Link to lists in cloud
+data_link='https://bliss-rdt.herokuapp.com/api/ble-data/'
+device_link='https://bliss-rdt.herokuapp.com/api/ble-device/'
+
+## calling createDeviceList function to get ble_device list data
+Tag_List=bleadv.createDeviceList(device_link)['TagList']
+data_fields=bleadv.createDeviceList(device_link)['data_fields']
+
+print 'No. of devices in whitelist are',len(Tag_List)
+print 'Keys in whitelist:',data_fields
+
+
+while (1):
+
+	## calling parseAdv to scan for ble devices, parse their adv packets and print out adv data
+	## in ble_data_parsed.txt
+	numTagsFound=bleadv.parseAdv(scantime,lib_path,advDataFile)
+	print('No. of advertisers scanned are: %d' %numTagsFound)
+
+	## update temp, humidity and timestamp in Tag_List based on the adv data
+	indexTagsUp=bleadv.tagDataUpdate(Tag_List,advDataFile,gatewayID,passAll)
+	print('No. of tags updated are: %d' %len(indexTagsUp))
+	#print 'Index of updated tags: ', indexTagsup[:]
+ 
+	## upload data (this needs to be optimized to upload only newly updated items)
+	upload_status=bleadv.bleDataUp(data_link,Tag_List,indexTagsUp,printUpData)
+	print 'All identified tags updated :', upload_status
+	
+	time.sleep(sleeptime)
