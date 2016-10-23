fcb8a80bc3ec34027aa1f97c8b9c7045c84931cc
Sameer Singh <sameersingh.mail@gmail.com>
2016-10-23 09:21:29 +0000

    blemaster_run_check.sh created online with Bitbucket

---
+++ launchers/blemaster_run_check.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#ps -ef | grep [b]lemaster > /dev/null
+COUNT=$(ps -aux | grep -c "python blemaster.py")
+echo $COUNT
+
+if [ $COUNT -gt 1 ] 
+then
+
+  echo "Process is running."
+
+else
+
+  echo "Process is not running."
+
+  echo "Replace this section with hci down-hci up and blemaster restart process"
+
+
+  sudo hciconfig hci0 down
+  sudo hciconfig hci0 up
+  
+  sudo /home/pi/ble_launcher.sh &
+  
+  sleep 25
+  
+  COUNT=$(ps -aux | grep -c "python blemaster.py")
+  echo $COUNT
+
+fi
