b65be3cb66f5790576109835cc5812b8b4187143
sameer <sameersingh.mail@gmail.com>
2017-04-28 13:49:13 +0000

    removed scheduled reboots; added scheduled service restarts

--- startup_config/mqttservice_launcher.sh
+++ startup_config/mqttservice_launcher.sh
@@ -4,8 +4,8 @@ sleep 10
 #cd /home/pi/tagbox/ble/hci_testruns/
 cd /home/pi/tagbox/ble/blegateway/
 
-sleep 10
+sleep 5
 
 #sudo python blemaster_test.py > /home/pi/log_blemaster_test.txt
 #sudo python blemaster_azure.py > /home/pi/tagbox/ble/hci_testruns/log_blemaster.txt
-sudo python mqttClient.py&
+sudo python mqttClient.py 
